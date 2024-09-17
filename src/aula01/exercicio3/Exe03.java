600da3a85e4f4a42aa5fdc39fce0e1e58aedb99a
alunojava-03 <alunojava-03@eldorados-MacBook-Pro.local>
2024-09-17 15:09:48 -0300

    Upando Exercício 3

---
+++ src/aula01/exercicio3/Exe03.java
@@ -0,0 +1,26 @@
+package aula01.exercicio3;
+import java.util.Scanner;
+
+public class Exe03 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        double primeiroNumero, segundoNumero, terceiroNumero;
+
+        System.out.println("---------Maior, Menor e Média de 3 Números---------");
+        System.out.print("Digite o primeiro número: ");
+        primeiroNumero = ler.nextInt();
+        System.out.print("Digite o segundo número: ");
+        segundoNumero = ler.nextInt();
+        System.out.print("Digite o terceiro número: ");
+        terceiroNumero = ler.nextInt();
+
+        double maior = Math.max(primeiroNumero, Math.max(segundoNumero, terceiroNumero));
+        double menor = Math.min(primeiroNumero, Math.min(segundoNumero, terceiroNumero));
+        double media = (primeiroNumero + segundoNumero + terceiroNumero) / 3;
+
+        System.out.println("O maior número entre os digitados é: " + maior);
+        System.out.println("O menor número entre os digitados é: " + menor);
+        System.out.println("A média aritmética dos números digitados é: " + media);
+
+    }
+}
