17a1f6f583ce166035d1c35cd313cf12c2fd35dd
alunojava-03 <alunojava-03@eldorados-MacBook-Pro.local>
2024-10-03 16:15:08 -0300

    upando exercício 22

---
+++ src/aula07/exercicio22/Exe22.java
@@ -0,0 +1,28 @@
+package aula07.exercicio21;
+
+import aula07.exercicio22.ValorInvalidoException;
+
+import java.util.Scanner;
+
+public class Exe22 {
+    public static void main(String[] args) {
+        maiorQueDez();
+    }
+
+    public static void maiorQueDez(){
+        Scanner ler = new Scanner(System.in);
+        try {
+            int numeroDigitado;
+            System.out.println("Digite um número maior ou igual a dez: ");
+            numeroDigitado = ler.nextInt();
+            if (numeroDigitado < 10){
+                throw new ValorInvalidoException("O número " + numeroDigitado + " é menor que 10!");
+            } else {
+                System.out.println("O número " + numeroDigitado + " é maior ou igual a 10.");
+            }
+        } catch (ValorInvalidoException e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+
+    }
+}
---
+++ src/aula07/exercicio22/ValorInvalidoException.java
@@ -0,0 +1,7 @@
+package aula07.exercicio22;
+
+public class ValorInvalidoException extends Exception{
+    public ValorInvalidoException(String msg){
+        super(msg);
+    }
+}
