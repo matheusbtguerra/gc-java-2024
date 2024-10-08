a799b4246c8da234791083660fab5c68613cdc3c
alunojava-03 <alunojava-03@eldorados-MacBook-Pro.local>
2024-10-08 16:57:41 -0300

    iniciando boss do mundo 01

---
+++ src/bossMundo01/GestaoMercenarios.java
@@ -0,0 +1,7 @@
+package bossMundo01;
+
+public class GestaoMercenarios {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/bossMundo01/Mercenario.java
@@ -0,0 +1,8 @@
+package bossMundo01;
+
+public class Mercenario {
+    String nome, classe;
+    int idade, salarioBase, avaliacaoDesempenho;
+
+
+}
